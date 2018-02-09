593b6514a106d46e04b7fad69c02c12d72bfcfd3
Raih07 <kirill.r.2010.stud@gmail.com>
2018-02-09 20:00:58 +0300

    обновил package.json, gukpfile.js, gitignore и спрайты. Пакеты не устанавливал

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,69 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+
+var imagemin = require("gulp-imagemin");
+
+var webp = require("gulp-webp");
+
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+
+var run = require("run-sequence");
+var del = require("del");
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function() {
+  return gulp.src("source/img/**/*.{png,jpg")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function() {
+  return gulp.src("source/img/icons/sprite-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+     }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function() {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+     ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "sourse/fonts/**/*.{woff,woff2}",
+    "sourse/img/**",
+    "sourse/js/**",
+    "sourse/*.html"
+  ], {
+    base: "sourse"
+  })
+    .pipe(gulp.dest("build"));
+});
+
 gulp.task("style", function() {
   gulp.src("source/less/style.less")
     .pipe(plumber())
@@ -14,13 +77,16 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.minify())
+    .pipe(gulp.rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("serve", function() {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -28,5 +94,16 @@ gulp.task("serve", ["style"], function() {
   });
 
   gulp.watch("source/less/**/*.less", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html"), ["html"]);
+});
+
+gulp.task("build", function(done) {
+  run(
+    "clean",
+    "copy"
+    "style",
+    "sprite",
+    "html",
+    done
+  );
 });
