b3b198fb1ccda88c67b805f4928a53d193e6b9d0
Raih07 <kirill.r.2010.stud@gmail.com>
2018-02-13 15:10:31 +0300

    разные фиксы

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,16 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
-
 var minify = require("gulp-csso");
 var rename = require("gulp-rename");
-
 var imagemin = require("gulp-imagemin");
-
 var webp = require("gulp-webp");
-
 var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
-
 var run = require("run-sequence");
 var del = require("del");
-
 var uglify = require("gulp-uglify");
-
 var htmlmin = require("gulp-htmlmin");
 
 gulp.task("minjs", function () {
@@ -62,7 +55,10 @@ gulp.task("html", function() {
     .pipe(posthtml([
       include()
      ]))
-    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
     .pipe(gulp.dest("build"));
 });
 
@@ -87,6 +83,7 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("source/css"))
     .pipe(minify())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css"))
